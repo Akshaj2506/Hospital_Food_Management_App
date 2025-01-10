9e5663f84f2b36a1e0f317de6a7013bc24760d5f
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2025-01-10 23:46:18 +0530

    removed slash from cors origin

--- food_management_backend/index.js
+++ food_management_backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.BACK_PORT
 connectDB()
 app.use(express.json());
 app.use(cors({
-   origin: ["https://hospital-food-management-app.vercel.app/"],
+   origin: ["https://hospital-food-management-app.vercel.app"],
    methods : ["POST", "GET", "PUT", "DELETE", "PATCH"],
    credentials: true
 }));
